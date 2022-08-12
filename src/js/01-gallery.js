6f9064b9b823062695c5e05486e245a8e88548f3
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2022-08-12 18:26:24 +0300

    change SimleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+// Change code below this line
+const galleryListRef = document.querySelector('.gallery');
+const createGallery = createGalleryList(galleryItems);
+
+galleryListRef.addEventListener('click', onClick);
+galleryListRef.insertAdjacentHTML('beforeend', createGallery);
+
+function createGalleryList(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"/>
+  </a>
+    </li>`;
+    })
+    .join('');
+}
+
+function onClick(evt) {
+  evt.preventDefault();
+}
+
+function createLiteBox() {
+  let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+  return lightbox;
+}
+createLiteBox();
