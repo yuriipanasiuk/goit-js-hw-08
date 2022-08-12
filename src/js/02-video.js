97a3ec4987b20aa74133521c25476262eb0c6a8e
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2022-08-12 20:17:06 +0300

    fix videoPlayer

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const videoDuration = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  localStorage.setItem('videoDuration', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = localStorage.getItem('videoDuration');
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
